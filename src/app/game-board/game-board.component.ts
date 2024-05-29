159c5f06644d99ac6968924f9511ac0d4d57159a
Ranga <ranga@Rangas-MacBook-Air.local>
2024-05-29 14:07:19 +0100

    created gamr-board component

---
+++ src/app/game-board/game-board.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-board',
+  templateUrl: './game-board.component.html',
+  styleUrls: ['./game-board.component.css']
+})
+export class GameBoardComponent {
+  squares: any[];
+  xIsNext: boolean;
+  winner: string;
+
+  constructor() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.xIsNext = true;
+    this.winner = null;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makeMove(idx: number) {
+    if (!this.squares[idx]) {
+      this.squares[idx] = this.player;
+      this.xIsNext = !this.xIsNext;
+    }
+
+    this.winner = this.calculateWinner();
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+}
